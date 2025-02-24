c562c40c3e7da33f2fd8f91da172c09793b35b5c
ervan-aegis <ervan@aegisnet.info>
2025-02-24 19:46:45 +0700

    penambahan logo di footer

--- src/app/components/footers/page.tsx
+++ src/app/components/footers/page.tsx
@@ -1,21 +1,34 @@
 import Image from 'next/image';
 import Logo from '@/assets/logo.jpeg';
+import React from "react";
 
 const Footer = () => {
     return (
         <div className="text-center mt-32">
             <p className="text-md font-medium md:text-base text-white">
                 Layanan Hotline Kesehatan Mental dan Bunuh Diri
-                <br />
+                <br/>
                 Dinas Kesehatan Provinsi Jakarta
             </p>
-            <Image
-                src={Logo}
-                alt="Logo Kemenkes RS Marzoeki Mahdi"
-                className="mx-auto mt-8"
-                width={100}
-                height={50}
-            />
+            <div className="flex items-center justify-center p-4 mb-8">
+                <div className="flex flex-row items-center space-x-2">
+                    <Image
+                        src={Logo}
+                        alt="Logo Kemenkes RS Marzoeki Mahdi"
+                        className="mx-auto"
+                        width={70}
+                        height={50}
+                    />
+                    <img src="https://dinkes.jakarta.go.id/assets/images/logo/jayaraya.png" alt="Logo" width={50}
+                         height={50}/>
+                    <img src="https://dinkes.jakarta.go.id/assets/images/logo/jaksehat.png" alt="Logo" width={50}
+                         height={50}/>
+                    <img src="https://dinkes.jakarta.go.id/assets/images/logo/rsuj.png" alt="Logo" width={60}
+                         height={70}/>
+                    <img src="https://dinkes.jakarta.go.id/assets/images/logo/puskesmas.png" alt="Logo" width={50}
+                         height={50}/>
+                </div>
+            </div>
         </div>
     )
 }
--- src/app/components/headers/page.tsx
+++ src/app/components/headers/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Image from 'next/image';
+
 
 const Header: React.FC = () => {
     return (
-        <h1 className="w-full text-center px-4 sm:px-6 md:px-8 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold mb-6 sm:mb-8 md:mb-10 lg:mb-12 text-green-900">
+        <h1 className="w-full text-center px-4 sm:px-6 md:px-8 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold mb-6 sm:my-8 md:my-10 lg:my-16 text-green-900">
             Apa yang bisa kami bantu hari ini?
         </h1>
     );
